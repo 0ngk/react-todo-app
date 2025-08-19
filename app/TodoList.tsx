91b0b9c5d9a37c73e74c0616e1c7160e5b9f5d15
Medo Rei <215371699+med0rei@users.noreply.github.com>
2025-08-19 16:19:49 +0900

    feat: add tasklist

---
+++ app/TodoList.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+export const TodoList = ({ todos }: { todos: Todo[] }) => {
+  return (
+    <div>
+      { todos.map(todo => <Todo key={todo.id} todo={todo}/>) }
+    </div>
+  );
+}
+
+export type Todo = {
+  id: string,
+  title: string,
+  description: string,
+  due?: Date,
+  isCompleted: boolean,
+}
+
+const Todo = ({todo}: { todo: Todo }) => {
+  return (
+    <div>
+      <TodoLeft isCompleted={todo.isCompleted} />
+      <TodoCenter title={todo.title} description={todo.description} due={todo.due} />
+      <TodoRight/>
+    </div>
+  );
+}
+
+const TodoLeft = ({ isCompleted }: { isCompleted: boolean }) => {
+  const handleOnChange = () => {}
+  
+  return (
+    <div>
+      <CheckBox onChange={handleOnChange} isChecked={isCompleted} />
+    </div>
+  );
+}
+
+const TodoCenter = (
+  { title, description, due }: { title: string, description: string, due?: Date }
+) => {
+  return (
+    <div>
+      <p>{title}</p>
+      <p>{description}</p>
+      <p>{due?.toString()}</p>
+    </div>
+  );
+}
+
+const TodoRight = () => {
+  const handleEdit = () => {}
+  const handleDelete = () => {}
+  return (
+    <div>
+      <Button onClick={handleEdit} value="Edit" />
+      <Button onClick={handleDelete} value="Delete" />
+    </div>
+  );
+}
+
+const CheckBox = ({ onChange, isChecked }: { onChange: () => void, isChecked: boolean }) => {
+  return (
+    <input type="checkbox" onChange={onChange} checked={isChecked} />
+  )
+}
+
+const Button = ({ onClick, value }: { onClick: () => void, value: string }) => {
+  return (
+    <>
+      <button onClick={onClick}>{value}</button>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+import { Todo, TodoList } from "./TodoList";
+import { Navigation } from "./Navigation";
+
 export default function Home() {
+  const dummyTodos: Todo[] = [
+    {
+      id: "id-1",
+      title: "水を2L飲む",
+      description: "ミネラルウォーター",
+      isCompleted: false,
+    },
+    {
+      id: "id-2",
+      title: "顔を洗う",
+      description: "洗顔料を使う",
+      isCompleted: true,
+    },
+  ];
   return (
     <>
       <Navigation/>
-      <TodoList/>
+      <TodoList todos={dummyTodos} />
     </>
   );
 }
